3a39906531fce693f609d7e45e791997453743e3
Tanner Lewis <lewijacn@amazon.com>
2024-05-31 17:33:57 -0400

    Minor bin update
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

---
+++ deployment/cdk/test-cdk/bin/app.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import {App} from 'aws-cdk-lib';
+import {StackComposer} from "../lib/stack-composer";
+
+const app = new App();
+const account = process.env.CDK_DEFAULT_ACCOUNT
+const region = process.env.CDK_DEFAULT_REGION
+
+new StackComposer(app, {
+    env: { account: account, region: region }
+});
